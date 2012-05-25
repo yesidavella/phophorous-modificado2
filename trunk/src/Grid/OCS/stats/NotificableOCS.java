4c693501a39b502008eefedc6140438bef8eb5c2
franklinalwinmartinez@gmail.com <franklinalwinmartinez@gmail.com@ac4cc498-7799-5a43-959d-79553ca1587e>
2012-05-25 04:30:39 +0000

    Parte 2 de vista OCS

---
+++ trunk/src/Grid/OCS/stats/NotificableOCS.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Grid.OCS.stats;
+
+import Grid.Entity;
+
+/**
+ *
+ * @author Frank
+ */
+public interface NotificableOCS 
+{
+    public void notifyNewCreatedOCS(Entity entitySource , Entity entityDestination);
+    
+    public void clean();
+    
+}
