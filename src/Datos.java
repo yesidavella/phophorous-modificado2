ba3e74c579c223d5e9e3944eac5499353ab870af
franklinalwinmartinez <franklinalwinmartinez@gmail.com>
2013-05-01 02:57:42 +0000

    API generacion de datos v.1

---
+++ src/Datos.java
@@ -0,0 +1,53 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author franklin
+ */
+public class Datos {
+
+    private Coeficiente coe1 = new Coeficiente(1, 10, 1);
+    private Coeficiente coe2 = new Coeficiente(1, 10, 1);
+    private Coeficiente coe3 = new Coeficiente(1, 10, 1);
+    private ArrayList<Double> combinacion;
+    private ArrayList<ArrayList<Double>> totalCombinacion = new ArrayList<ArrayList<Double>>();
+
+    public Datos() {
+
+
+        iterarCoerficientes();
+    }
+
+    private void iterarCoerficientes() {
+
+        int ciclos =0;
+        while (coe1.hasNext()) 
+        {
+            coe1.next(); 
+            while (coe2.hasNext()) 
+            {
+                coe2.next(); 
+                while (coe3.hasNext()) 
+                {
+                    ciclos++;
+                    System.out.println("Ciclo:"+ciclos +" Val coe1:" +coe1.getValor()  + " Val coe2:" + coe2.getValor() + " Val coe3:" + coe3.next());
+                    combinacion = new ArrayList<Double>();
+                    combinacion.add(coe1.getValor());
+                    combinacion.add(coe2.getValor());
+                    combinacion.add(coe3.getValor());
+                    
+                    totalCombinacion.add(combinacion);
+                }
+                coe3.reset();
+            }
+            coe2.reset();
+        }
+        System.out.println("Total ciclos:"+ciclos);
+    }
+}
